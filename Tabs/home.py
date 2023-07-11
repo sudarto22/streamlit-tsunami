384359925e49ce773c769e892b8ddf94048e5d9e
sudarto22 <52229670+sudarto22@users.noreply.github.com>
2023-07-11 10:34:14 +0700

    prediksi

---
+++ Tabs/home.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+#@st.cache_data
+
+def app():
+    #judul halaman aplikasi
+    #st.title("Aplikasi Prediksi Tsunami Gempa Bumi Dengan Algoritma Pohon Keputusan C45")
+    st.markdown("<h1 style='text-align: center;'>Aplikasi Prediksi Tsunami Gempa Bumi Dengan Algoritma Pohon Keputusan C45</h1>", unsafe_allow_html=True)
+    
