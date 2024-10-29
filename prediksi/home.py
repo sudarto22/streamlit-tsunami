5358422003133b7357912541dd8ed2ac2f88e92c
sudarto22 <52229670+sudarto22@users.noreply.github.com>
2024-10-29 10:31:39 +0700

    Delete prediksi directory

--- prediksi/home.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-#@st.cache_data
-
-def app():
-    #judul halaman aplikasi
-    #st.title("Aplikasi Klasifikasi Tsunami Pasca Gempa Bumi Dengan Teknik Stacking Ensemble")
-    st.markdown("<h1 style='text-align: center;'>Aplikasi Klasifikasi Tsunami Pasca Gempa Bumi Dengan Teknik Stacking Ensemble</h1>", unsafe_allow_html=True)
-    
